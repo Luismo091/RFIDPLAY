e839a37416ddaa9257394fb8782e349f0d931d7f
Luis Fernando Mora Aroca <101841546+Luismo091@users.noreply.github.com>
2023-10-22 13:18:51 -0500

    Readme updated

--- main/conexion.php
+++ main/conexion.php
@@ -1,7 +1,7 @@
 <?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
+$hostname = "rfidplya-datacenter.mysql.database.azure.com";
+$username = "rfidMaster";
+$password = "\$SecureMySQL2023@";
 $database = "rfidplay";
 
 $start_time = microtime(true);
@@ -12,7 +12,6 @@ if ($mysqli->connect_error) {
     die("Error en la conexión: " . $mysqli->connect_error);
 }
 $end_time = microtime(true);
-
 // Calcular la latencia
 $latency = round(($end_time - $start_time) * 1000, 2); // Convertir a milisegundos
 ?>
